ecae94c0d6f2a2a9d09f4110b9291e3e4c1c9580
Koji <yourpartner2021@gmail.com>
2025-02-15 08:35:24 +0900

    Initial Commit

---
+++ Marubastu/MarubastuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubastuApp.swift
+//  Marubastu
+//
+//  Created by 田中康志 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarubastuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
